05355ead60ee18a0534d1fe9d4d5278acaf39efc
ashwini-8 <ashwinip309@gmail.com>
2020-11-20 14:10:10 +0530

    Ädded:regex first name use case

--- regex.sh
+++ regex.sh
@@ -1,1 +1,15 @@
 echo "regex assignment used cases"
+
+
+echo "#################################"
+
+
+echo "First Name : "
+read name
+pat="^[A-Z a-z]{2,}"
+if [[ $name == $pat ]]
+then
+  echo "$name is valid Name"
+else
+  echo "Not valid Name"
+fi
