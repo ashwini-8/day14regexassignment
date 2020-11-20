c7c3f2dbae4c421656a96c54106d932cb0e56ba6
ashwini-8 <ashwinip309@gmail.com>
2020-11-20 14:39:51 +0530

    Added:regex Emailid use case

--- regex.sh
+++ regex.sh
@@ -1,4 +1,22 @@
-echo "Lasr Name : "
+echo "regex assignment used cases"
+
+
+echo "#################################"
+
+
+echo "First Name : "
+read name
+pat="^[A-Z a-z]{2,}"
+if [[ $name == $pat ]]
+then
+  echo "$name is valid Name"
+else
+  echo "Not valid Name"
+fi
+
+echo "################################"
+
+echo "Last Name : "
 read name
 pat="^[A-Z a-z]{3,}"
 if [[ $name == $pat ]]
@@ -6,4 +24,18 @@ then
   echo "$name is valid Name"
 else
   echo "Not valid Name"
+fi
+
+
+echo "###################################"
+
+
+echo "Enter Email ID : "
+read email
+pat2="^[0-9a-zA-Z]+([.\-_+][0-9a-zA-Z]+)@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})$"
+if [[ $email =~ $pat2 ]]
+then
+  echo "$email is valid Email"
+else
+  echo "Not valid Email"
 fi
